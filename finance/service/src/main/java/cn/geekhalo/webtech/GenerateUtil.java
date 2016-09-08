d09df9faab4e2cfc41c7232dff4de7a5d90bcd77
SmartGim <419771681@qq.com>
2016-09-08 16:56:25 +0800

    generator class

---
+++ finance/service/src/main/java/cn/geekhalo/webtech/GenerateUtil.java
@@ -0,0 +1,16 @@
+package cn.geekhalo.webtech;
+/**
+ * 
+ * @author GIM
+ *
+ */
+public class GenerateUtil {
+	public static void main(String[] args) {
+		
+	}
+	
+	public void generateCode(){
+		String s = System.getProperty("user.dir");
+		System.out.println(s);
+	}
+}
