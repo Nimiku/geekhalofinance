9ea70ccff5ad9f1c416939df1a4e61b4dbfcbc5c
SmartGim <419771681@qq.com>
2016-09-08 17:16:17 +0800

    dom4j

--- finance/service/src/main/java/cn/geekhalo/webtech/GenerateUtil.java
+++ finance/service/src/main/java/cn/geekhalo/webtech/GenerateUtil.java
@@ -1,4 +1,15 @@
 package cn.geekhalo.webtech;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Iterator;
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+
 /**
  * 
  * @author GIM
@@ -10,7 +21,23 @@ public class GenerateUtil {
 	}
 	
 	public void generateCode(){
-		String s = System.getProperty("user.dir");
-		System.out.println(s);
+		String workspace = System.getProperty("user.dir");
+		File f = new File(workspace+"/generator/mybatis_code_autoGenerator/mygenerator_mysql.xml");
+		SAXReader reader = new SAXReader();
+        Document doc = reader.read(f);
+        List list = doc.selectNodes("/generatorConfiguration/context");
+        Iterator iter = list.iterator();
+        while(iter.hasNext()){
+        	 Element elem = (Element)iter.next();
+             Iterator iterElem = elem.elementIterator("table");
+             if(iterElem.hasNext()){
+                 Element remTitle = (Element)iterElem.next();
+                 System.out.println(remTitle.getText());
+             }
+        }
+        FileWriter newFile = new FileWriter(new File(workspace+"/generator/mybatis_code_autoGenerator/mygenerator_mysql1.xml"));
+        XMLWriter newWriter = new XMLWriter(newFile);
+        newWriter.write(doc);
+        newWriter.close();
 	}
 }
